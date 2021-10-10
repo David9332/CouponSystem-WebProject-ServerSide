7a0e3c0fd79ed0216b210e6b3b40049efbeab014
David9332 <davger404@gmail.com>
2021-10-10 20:03:47 +0300

    released configuration in the program's thread.

--- src/main/java/com/example/SpringProject/DailyJobThread/DailyJobThread.java
+++ src/main/java/com/example/SpringProject/DailyJobThread/DailyJobThread.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-//@Configuration
+@Configuration
 @EnableScheduling
 @RequiredArgsConstructor
 public class DailyJobThread {
